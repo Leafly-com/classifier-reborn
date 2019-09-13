a87d2f90b2711050b6b0cd9946fb6041039b6056
Alex Sleighter <alex.sleighter@leafly.com>
2019-09-13 14:59:02 -0400

    Allow namespacing redis keys  - The avoids the need to use a separate redis db for each review type

--- lib/classifier-reborn/backends/bayes_redis_backend.rb
+++ lib/classifier-reborn/backends/bayes_redis_backend.rb
@@ -6,7 +6,8 @@ require_relative 'no_redis_error'
 module ClassifierReborn
   # This class provides Redis as the storage backend for the classifier data structures
   class BayesRedisBackend
-    # The class can be created with the same arguments that the redis gem accepts
+    # The class can be created with the same arguments that the redis gem accepts. 
+    # An optional `key_prefix` argument will be prepended to all redis keys. 
     # E.g.,
     #      b = ClassifierReborn::BayesRedisBackend.new
     #      b = ClassifierReborn::BayesRedisBackend.new host: "10.0.1.1", port: 6380, db: 2
@@ -26,6 +27,7 @@ module ClassifierReborn
     #   tcp_keepalive:      0
     #   reconnect_attempts: 1
     #   inherit_socket:     false
+    #   key_prefix:         nil
     def initialize(options = {})
       begin # because some people don't have redis installed
         require 'redis'
@@ -33,33 +35,38 @@ module ClassifierReborn
         raise NoRedisError
       end
 
+      @key_prefix = options.delete(:key_prefix)&.concat(":") || ""
       @redis = Redis.new(options)
-      @redis.setnx(:total_words, 0)
-      @redis.setnx(:total_trainings, 0)
+      @redis.setnx(key_for(:total_words), 0)
+      @redis.setnx(key_for(:total_trainings), 0)
+    end
+
+    def key_for(key)
+      "#{@key_prefix}#{key}"
     end
 
     def total_words
-      @redis.get(:total_words).to_i
+      @redis.get(key_for(:total_words)).to_i
     end
 
     def update_total_words(diff)
-      @redis.incrby(:total_words, diff)
+      @redis.incrby(key_for(:total_words), diff)
     end
 
     def total_trainings
-      @redis.get(:total_trainings).to_i
+      @redis.get(key_for(:total_trainings)).to_i
     end
 
     def update_total_trainings(diff)
-      @redis.incrby(:total_trainings, diff)
+      @redis.incrby(key_for(:total_trainings), diff)
     end
 
     def category_training_count(category)
-      @redis.hget(:category_training_count, category).to_i
+      @redis.hget(key_for(:category_training_count), category).to_i
     end
 
     def update_category_training_count(category, diff)
-      @redis.hincrby(:category_training_count, category, diff)
+      @redis.hincrby(key_for(:category_training_count), category, diff)
     end
 
     def category_has_trainings?(category)
@@ -67,41 +74,43 @@ module ClassifierReborn
     end
 
     def category_word_count(category)
-      @redis.hget(:category_word_count, category).to_i
+      @redis.hget(key_for(:category_word_count), category).to_i
     end
 
     def update_category_word_count(category, diff)
-      @redis.hincrby(:category_word_count, category, diff)
+      @redis.hincrby(key_for(:category_word_count), category, diff)
     end
 
     def add_category(category)
-      @redis.sadd(:category_keys, category)
+      @redis.sadd(key_for(:category_keys), category)
     end
 
     def category_keys
-      @redis.smembers(:category_keys).map(&:intern)
+      @redis.smembers(key_for(:category_keys)).map(&:intern)
     end
 
     def category_word_frequency(category, word)
-      @redis.hget(category, word).to_i
+      @redis.hget(key_for(category), word).to_i
     end
 
     def update_category_word_frequency(category, word, diff)
-      @redis.hincrby(category, word, diff)
+      @redis.hincrby(key_for(category), word, diff)
     end
 
     def delete_category_word(category, word)
-      @redis.hdel(category, word)
+      @redis.hdel(key_for(category), word)
     end
 
     def word_in_category?(category, word)
-      @redis.hexists(category, word)
+      @redis.hexists(key_for(category), word)
     end
 
     def reset
-      @redis.flushdb
-      @redis.set(:total_words, 0)
-      @redis.set(:total_trainings, 0)
+      @redis.scan_each(match: "#{@key_prefix}?") do |key|
+        @redis.unlink(key)
+      end
+      @redis.set(key_for(:total_words), 0)
+      @redis.set(key_for(:total_trainings), 0)
     end
   end
 end
